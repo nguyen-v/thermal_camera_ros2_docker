e2f93e40e5e196149c49bae3301f8916989eeb50
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-02-26 18:14:22 +0100

    mount device directlyl and remove privileged flag

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker run -it --privileged --rm -v=/dev/camera:/dev/camera thermal_camera_ros2:latest \
+docker run -it --rm \
+  --device=/dev/camera/video_thermal:/dev/camera/video_thermal \
+  thermal_camera_ros2:latest \
   ros2 launch thermal_camera thermal_camera_launch.py colormap:=2 hud:=False orientation:=270 device:=/dev/camera/video_thermal
