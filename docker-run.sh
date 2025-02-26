beb2cb575608bead71c380255dc48777258d92b6
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-02-26 18:17:38 +0100

    simplify dev name

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run -it --rm \
-  --device=/dev/camera/video_thermal:/dev/camera/video_thermal \
+  --device=/dev/camera/video_thermal \
   thermal_camera_ros2:latest \
   ros2 launch thermal_camera thermal_camera_launch.py colormap:=2 hud:=False orientation:=270 device:=/dev/camera/video_thermal
